2b7065d522585d5516d871772e8402bfa7d5e80e
Arr000 <arash_shahinzadeh@hotmail.com>
2021-03-31 17:11:44 +0200

    La till två klasser

---
+++ TestingForGroupProject/GruppTest/src/grupptest/BloggGUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupptest;
+
+/**
+ *
+ * @author nene5
+ */
+public interface BloggGUI {
+    
+}
